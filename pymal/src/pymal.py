687f2c3dc6116e49ccfd041bc19d84ab0cd7cd1e
James Elder <james.elder@utah.edu>
2015-01-15 13:47:00 -0700

    initialize

---
+++ pymal/src/pymal.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "u0074071"
+__date__ = "$Jan 15, 2015 1:46:27 PM$"
+
+if __name__ == "__main__":
+    print ("Hello World")
